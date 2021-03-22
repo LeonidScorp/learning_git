d31d037e7ef511e67c1e6dfcdfd4e206bf9e6db9
LeonidScorp <fishchukl28@gmail.com>
2021-03-22 11:31:41 +0200

    Altered auth

--- auth.js
+++ auth.js
@@ -2,6 +2,6 @@ function authenticate (login, password) {
   if (login == 'login' && password == 'password') {
     return 'You were logged in';
   } else {
-    return 'Login is incorrect';
+    return 'Login or password is incorrect';
   }
 }
